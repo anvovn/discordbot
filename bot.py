e3a295eeba830928cbc6590c4ca3ab59e0c0fc33
An Vo <ovnaa05@gmail.com>
2025-04-12 17:53:39 -0700

    Detect and reply to user input

--- bot.py
+++ bot.py
@@ -2,17 +2,37 @@
 import discord
 import responses
 
-# Client
+# Get bot token
+TOKEN = 'MTM2MDY5NzY3OTg3MjUyODU5Ng.GykOFH.Ft91vod6vef7pbJvBxWHLbg9-wO4vce1Hfj2vc'
+
+# Getting user input
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.get_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+    
+    except Exception as e:
+        print(e)
+
+# Client 
 class MyClient(discord.Client):  # Hover over discord.Client to see all parameters that can be passed for line 18 - "client = MyClient"
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
     async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+        if message.author == MyClient.user:
+            return
+
+        if message.content[0] == '?':
+            message.content = message.content[1:]
+            await send_message(message, message.content, is_private=True)   # If a user sends a message starting with ?, the bot will reply in DM
+        else:
+            await send_message(message, message.content, is_private=False)  # Otherwise reply directly in the channel
+
+        print(f'Message from {message.author}: {message.content}, in channel: {message.channel}')
 
 # Define run function for main.py
 def run_discord_bot():
-    TOKEN = 'MTM2MDY5NzY3OTg3MjUyODU5Ng.GykOFH.Ft91vod6vef7pbJvBxWHLbg9-wO4vce1Hfj2vc'
     intents = discord.Intents.default()
     intents.message_content = True
     client = MyClient(intents=intents)
--- responses.py
+++ responses.py
@@ -3,13 +3,15 @@ import responses
 
 # Chat bot features
 def get_response(message: str) -> str:
-    p_message = message.lower()
-
     '''
     Example usage:
-
     if p_message == 'hello':
         return f'Hello {p_message.author}!'
     '''
+
+    p_message = message.lower()
+
+    if p_message == "!help":
+        return "Commands: "
     
     return None
