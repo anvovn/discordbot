282bac684a6fe120359bca5ceced99a6c6ba65c9
An Vo <ovnaa05@gmail.com>
2025-05-08 15:35:55 -0700

    Cogs added, main updated

--- main.py
+++ main.py
@@ -2,22 +2,30 @@
 import responses
 import discord
 from discord.ext import commands
+import os
+import asyncio
 
 # Initialize bot
 TOKEN = 'MTM2MDY5NzY3OTg3MjUyODU5Ng.GXOaZJ.0ZbdA4C91ndaJv_qSuw-YA9daq3XVCTgQI_HIg'
 intents = discord.Intents.default()
 intents.message_content = True
-client = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 # Bot client
-@client.event
+@bot.event
 async def on_ready():
     print("Bot ready")
 
-@client.command()
-async def info(ctx):
-    await ctx.send("Commands: !help, ")
+async def load():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"cogs.{filename[:-3]}")
+
+async def main():
+    async with bot:
+        await load()
+        await bot.start(TOKEN)
 
 # Run bot
 if __name__ == '__main__':
-    client.run(TOKEN)
+    asyncio.run(main())
