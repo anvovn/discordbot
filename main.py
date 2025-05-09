0782de68512e2e351410f7e8a746d35661cffefd
Tyler Payne <tylerpayne@Tylers-MacBook-Air.local>
2025-05-09 14:46:29 -0700

    Your message here

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ import asyncio
 TOKEN = 'MTM2MDY5NzY3OTg3MjUyODU5Ng.GXOaZJ.0ZbdA4C91ndaJv_qSuw-YA9daq3XVCTgQI_HIg'
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="/", intents=intents)
+GUILD_ID = discord.Object(id=1360030609841590425)
 
 # Bot client
 @bot.event
 async def on_ready():
-    print("Bot ready")
+    await bot.tree.sync()
+    print(f"{bot.user} is ready (ID: {bot.user.id})")
 
 async def load():
     for filename in os.listdir("./cogs"):
