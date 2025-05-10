cd2b6a28688fd6356e67bfb06e3065da060ba2cd
An Vo <ovnaa05@gmail.com>
2025-05-09 17:03:54 -0700

    test

--- main.py
+++ main.py
@@ -6,16 +6,21 @@ import asyncio
 
 # Initialize bot
 TOKEN = 'MTM2MDY5NzY3OTg3MjUyODU5Ng.GXOaZJ.0ZbdA4C91ndaJv_qSuw-YA9daq3XVCTgQI_HIg'
+GUILD_ID = discord.Object(id=1360030609841590425)
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="/", intents=intents)
-GUILD_ID = discord.Object(id=1360030609841590425)
+bot = commands.Bot(command_prefix="!", intents=intents)
+bot_status = discord.Game("/help for more")
 
 # Bot client
 @bot.event
 async def on_ready():
     await bot.tree.sync()
+    await change_bot_status()
     print(f"{bot.user} is ready (ID: {bot.user.id})")
+
+async def change_bot_status():
+    await bot.change_presence(activity=bot_status)
 
 async def load():
     for filename in os.listdir("./cogs"):
