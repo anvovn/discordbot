d9d9192bf54ba4ba0fdfba52fb535ce3383d259f
An Vo <120775630+anvovn@users.noreply.github.com>
2025-06-06 22:07:29 -0700

    final changes

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from discord.ext import commands
 import os
 import asyncio
 from dotenv import load_dotenv
-from keep_alive import keep_alive
+from keepup import keepup
 
 # Initialize bot
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN") # Insert token here
-keep_alive()
+keepup()
 
 intents = discord.Intents.default()
 intents.message_content = True
